672953ee11da6fac0ebe7ffe9b593f877f532799
ArtsiomKushnir <akushnir@copyright.com>
2017-08-11 20:38:11 +0300

    added Mail sender implementation

---
+++ WEBAPPS-SHOP/paperki-shop/src/main/java/com/kushnir/paperki/webapp/paperki/shop/mail/Mailer.java
@@ -0,0 +1,85 @@
+package com.kushnir.paperki.webapp.paperki.shop.mail;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class Mailer {
+
+    private static final Logger LOGGER = LogManager.getLogger(Mailer.class);
+
+    @Autowired
+    private MailSender mailSender;
+
+    @Autowired
+    private SimpleMailMessage templateMessage;
+
+    @Value("${mail.system.sender}")
+    private String SYSTEM_EMAIL_ADDRESS;
+
+    @Value("${mail.developer.recipient}")
+    private String DEVELOPER_EMAIL_ADDRESS;
+
+    @Value("${mail.support.recipient}")
+    private String SUPPORT_SERVICE_EMAIL_ADDRES;
+
+    @Value("${mail.user.sender}")
+    private String USER_SERVICE_EMAIL_ADDRESS;
+
+    @Value("${mail.notification.sender}")
+    private String NOTIFICATION_SERVICE_EMAIL_ADDRESS;
+
+    private String[] supportResipients = new String[] {
+            SUPPORT_SERVICE_EMAIL_ADDRES,
+            DEVELOPER_EMAIL_ADDRESS
+    };
+
+    public void fromUserServiceEmail(String toEmail, String text) {
+        sendMail(toEmail, USER_SERVICE_EMAIL_ADDRESS, text);
+    }
+
+    public void fromSystemEmail(String toEmail, String text) {
+        sendMail(toEmail, SYSTEM_EMAIL_ADDRESS, text);
+    }
+
+    public void fromSupportEmail(String toEmail, String text) {
+        sendMail(toEmail, SUPPORT_SERVICE_EMAIL_ADDRES, text);
+    }
+
+    public void toSupportMail(String text) {
+        sendMail(supportResipients, SYSTEM_EMAIL_ADDRESS, text);
+    }
+
+    private void sendMail(String to, String from, String text) {
+        LOGGER.debug("Sending text email from({}) to ({}) >>> ", from, to);
+        templateMessage.setTo(to);
+        templateMessage.setFrom(from);
+        templateMessage.setText(text);
+        try {
+            mailSender.send(templateMessage);
+        } catch (Exception e) {
+            LOGGER.error(">>> FAIL SENDING EMAIL TO: {} \n{}", to, e.getMessage());
+        }
+        LOGGER.debug(">>> E-MAIL SUCCESSFULLY SENT");
+
+    }
+
+    private void sendMail(String[] to, String from, String text) {
+        LOGGER.debug("Sending text email from({}) to ({}) >>> ", from, to);
+        templateMessage.setTo(to);
+        templateMessage.setFrom(from);
+        templateMessage.setText(text);
+        try {
+            mailSender.send(templateMessage);
+        } catch (Exception e) {
+            LOGGER.error(">>> FAIL SENDING EMAIL TO: {} \n{}", to, e.getMessage());
+        }
+        LOGGER.debug(">>> E-MAIL SUCCESSFULLY SENT");
+
+    }
+}
--- WEBAPPS-SHOP/paperki-shop/src/main/java/com/kushnir/paperki/webapp/paperki/shop/schedules/UpdaterCSV.java
+++ WEBAPPS-SHOP/paperki-shop/src/main/java/com/kushnir/paperki/webapp/paperki/shop/schedules/UpdaterCSV.java
@@ -1,31 +1,34 @@
 package com.kushnir.paperki.webapp.paperki.shop.schedules;
 
 import com.kushnir.paperki.sevice.CategoryBean;
+
+import com.kushnir.paperki.webapp.paperki.shop.mail.Mailer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class UpdaterCSV {
 
-    private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss sss");
     private static final Logger LOGGER = LogManager.getLogger(UpdaterCSV.class);
 
     @Autowired
     CategoryBean categoryBean;
 
-    public void writeCurrentTime() {
-        Date now = new Date();
-        String nowString = df.format(now);
-        LOGGER.debug("Now is: {}", nowString);
-    }
+    @Autowired
+    Mailer mailer;
 
     public void catalogUpdate() throws IOException {
         LOGGER.debug("===== Started Catalog update ==== >>> ");
-        categoryBean.getCategoriesFromCSV();
+    }
+
+    public void productUpdate() {
+        LOGGER.debug("===== Started Products update ==== >>> ");
+    }
+
+    public void pricesUpdate() {
+        LOGGER.debug("===== Started Prices update ==== >>> ");
     }
 }
