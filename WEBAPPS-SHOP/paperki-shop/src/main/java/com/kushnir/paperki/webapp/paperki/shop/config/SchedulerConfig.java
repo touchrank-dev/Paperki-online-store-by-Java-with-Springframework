08ae8175e3f20ea2af1a69015cb13f4167cf05c7
ArtsiomKushnir <akushnir@copyright.com>
2017-08-11 12:26:32 +0300

    updated Scheduler configuration

--- WEBAPPS-SHOP/paperki-shop/src/main/java/com/kushnir/paperki/webapp/paperki/shop/config/SchedulerConfig.java
+++ WEBAPPS-SHOP/paperki-shop/src/main/java/com/kushnir/paperki/webapp/paperki/shop/config/SchedulerConfig.java
@@ -3,8 +3,9 @@ package com.kushnir.paperki.webapp.paperki.shop.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Configuration
-@EnableScheduling
+/*@Configuration
+@EnableScheduling*/
+@Deprecated
 public class SchedulerConfig {
 
 }
