fb80d691199c68013e3728ebcf4a0aaac86a142e
ArtsiomKushnir <artsiomkush@gmail.com>
2017-12-13 14:20:27 +0300

    added login functionality to admin app

--- WEBAPPS-ADMIN/paperki-admin/src/main/java/com/kushnir/paperki/admin/controllers/app/MainController.java
+++ WEBAPPS-ADMIN/paperki-admin/src/main/java/com/kushnir/paperki/admin/controllers/app/MainController.java
@@ -1,16 +1,20 @@
 package com.kushnir.paperki.admin.controllers.app;
 
+import com.kushnir.paperki.model.user.User;
+import com.kushnir.paperki.model.user.UserType;
 import org.h2.util.IOUtils;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/")
@@ -18,6 +22,13 @@ public class MainController {
 
     @GetMapping()
     public String mainPage() {
+        return "index";
+    }
+
+    @PostMapping()
+    public String login(@RequestParam HashMap<String, String> attributes, HttpSession httpSession) {
+        User user = (User) httpSession.getAttribute("user");
+        login(attributes.get("login"), attributes.get("password"), user);
         return "index";
     }
 
@@ -29,5 +40,24 @@ public class MainController {
         IOUtils.copy(is, response.getOutputStream());
         response.flushBuffer();
         is.close();
+    }
+
+    @ModelAttribute("user")
+    public User setUser(HttpSession httpSession, HttpServletRequest req) {
+        if (req.getRequestedSessionId() != null) {
+            User user = (User) httpSession.getAttribute("user");
+            if (user == null) {
+                user = new User();
+                httpSession.setAttribute("user", user);
+            }
+            return user;
+        } else return null;
+    }
+
+    private void login(String login, String password, User user) {
+        if (login.equalsIgnoreCase("admin")
+                && password.equalsIgnoreCase("Pap111111")) {
+            user.setUserType(UserType.ADMIN);
+        }
     }
 }
--- WEBAPPS-ADMIN/paperki-admin/src/main/webapp/WEB-INF/view/resources/js/app.js
+++ WEBAPPS-ADMIN/paperki-admin/src/main/webapp/WEB-INF/view/resources/js/app.js
@@ -1,9 +1,4 @@
 document.onload = load();
 
 function load() {
-    alert('loaded');
-}
-
-function login() {
-
 }
